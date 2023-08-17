7d2d793857227f0337b648e82efab6b3fee74203
Shyamala30 <shymalamandagalale@gmail.com>
2023-08-17 18:31:58 +0530

    Ch.java

---
+++ Ch.java
@@ -0,0 +1,15 @@
+class Ch 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("hey hiii");
+		try {
+			int a = 1 / 0;
+		}
+		catch (Exception e)
+		{
+			System.out.println("exception handled");
+		}
+	}
+}
